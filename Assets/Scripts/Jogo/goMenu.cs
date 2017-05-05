5e891e3ae9e706e201487c85ba4d04248fdf5283
aamandachaves <a.amandachaves@outlook.com>
2017-05-05 13:55:51 -0400

    fade funcionando

--- Assets/Scripts/Jogo/goMenu.cs
+++ Assets/Scripts/Jogo/goMenu.cs
@@ -4,12 +4,22 @@ using UnityEngine.SceneManagement;
 
 public class goMenu : MonoBehaviour {
 
-    public GameObject fade;
-    
+    public GameObject fade, fade2;
+
+    private void Update()
+    {
+        Invoke("tirarFade", 1.5f);
+    }
+
     public void voltarMenu()
     {
         fade.SetActive(true);
         Invoke("voltar", 1.5f);
+    }
+
+    void tirarFade()
+    {
+        fade2.SetActive(false);
     }
 
     void voltar()
